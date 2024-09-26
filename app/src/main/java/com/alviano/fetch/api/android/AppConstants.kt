e921aba4efdefacd21173dcf051246970088a94d
MDAlviano <mdalviano.21122@gmail.com>
2024-09-26 21:16:30 +0700

    first commit

---
+++ app/src/main/java/com/alviano/fetch/api/android/AppConstants.kt
@@ -0,0 +1,5 @@
+package com.alviano.fetch.api.android
+
+object AppConstants {
+    val API_KEY = "zyo4fzZ763f/HsJmoU/7NQ==UWoUDebl1JKy8sPl"
+}
---
+++ app/src/main/java/com/alviano/fetch/api/android/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.alviano.fetch.api.android
+
+import android.net.ConnectivityManager
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alviano.fetch.api.android.networking.NetworkChecker
+import com.alviano.fetch.api.android.networking.RemoteApi
+
+class MainActivity : AppCompatActivity() {
+
+    private val networkChecker by lazy {
+        NetworkChecker(getSystemService(ConnectivityManager::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        RemoteApi().getFact()
+
+    }
+}
